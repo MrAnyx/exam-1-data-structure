6cdb8793f86481a87f24caca9c16b023c9f50ff5
MrAnyx <1ptitigre@gmail.com>
2021-10-19 23:23:44 -0400

    ✨ Added get(i, j) function

---
+++ Exercice 5/question2.py
@@ -0,0 +1,53 @@
+# Author : Robin B
+
+import math
+import numpy as np
+
+compact_matrix = [
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    "X",
+    " ",
+    "X",
+    " ",
+]
+
+# TODO revoir algo en utilisant le tableau et non la matrice
+def get(i, j):
+    global compact_matrix
+    tab_length = len(compact_matrix)
+    n = int((tab_length + 6) / 5)
+    if j in [0, math.floor(n / 2), n - 1]:
+        return "X"
+
+    else:
+        if (j > 0 and j < math.floor(n / 2) and i == 0) or (
+            j > math.floor(n / 2) and j < n - 1 and i == n - 1
+        ):
+            return "X"
+
+        else:
+            return " "
+
+
+n = 5
+matrix = [[" " for i in range(n)] for i in range(n)]
+
+for j in range(n):
+    for i in range(n):
+        matrix[j][i] = get(i, j)
+
+print(np.matrix(matrix))
